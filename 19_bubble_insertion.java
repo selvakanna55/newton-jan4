669190eca4bcbbeff2f986dd7c114cc9718876e7
selvakanna55 <selvakanna2409@gmail.com>
2022-02-14 18:23:07 +0530

    add

---
+++ 19_bubble_insertion.java
@@ -0,0 +1,39 @@
+class Main{
+
+  static void bubbleSort(int arr[], int n){
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n-1-i;j++){
+                if(arr[j]>arr[j+1]){
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+            }
+        }
+    }
+
+  static void insertionSort(int arr[], int n){
+    for(int i=0;i<n;i++){
+      int currValue = arr[i];
+      int hole = i;
+
+      while(hole>=1 && arr[hole-1]>currValue){
+        arr[hole] = arr[hole-1];
+        hole--;
+      }
+      arr[hole] = currValue;
+    }
+
+  }
+
+  public static void main(String args[]){
+    System.out.println("Start");
+    int arr[] = {11, 9, 8, 6, 101, 3, 1, -10, -99};
+    int n = arr.length;
+    insertionSort(arr, n);
+    //print array
+    for(int i=0;i<n;i++)
+      System.out.print(arr[i]+ " ");
+    System.out.println("End"); 
+    }
+}
