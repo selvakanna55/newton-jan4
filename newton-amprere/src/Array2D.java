9c65d0554b262ee54f7a3cb362e11d0b8bd6f0a1
selvakanna55 <selvakanna2409@gmail.com>
2022-04-04 18:10:49 +0530

    add

---
+++ newton-amprere/src/Array2D.java
@@ -0,0 +1,14 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Array2D {
+    public static void main(String[] args) {
+        int arr1[] = {1,2,3};
+        Collections.reverse(Arrays.asList(arr1));
+        System.out.println(Arrays.toString(arr1));
+
+    }
+}
+
